324e7c75c743d3aeaa11d005be84ce918ee49782
Denys <Denys@DESKTOP-SAAJBBA>
2016-06-05 18:54:07 +0300

    Add Styles

---
+++ SameBirthday/Utils/DateFormer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace SameBirthday.Utils
+{
+    public static class DateFormer
+    {
+        public static string Get(int day, int month, int year)
+        {
+            var date = new DateTime(year, month, day);
+            // return date.ToString("dd MMMM yyyy", CultureInfo.InvariantCulture);
+            return date.ToString("dd MMMM yyyy", new CultureInfo("ru-RU"));
+        }
+    }
+}
